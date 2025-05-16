dd577d26f39470d842ce4d510cea059649ab02fe
DanielSerio <dserio.d@gmail.com>
2025-05-16 10:48:59 -0400

    applied dimensions to workspace

--- src/modules/Workspace/components/Workspace.tsx
+++ src/modules/Workspace/components/Workspace.tsx
@@ -16,35 +16,44 @@ function WorkspaceComponent(
   const imageLayerSet = useLayerSet({ parent });
   const textLayerSet = useLayerSet({ parent });
 
+  const stored = localStorage.getItem("dims");
+  const dims = stored ? JSON.parse(stored) : null;
+
   return (
     <Box className="workspace" ref={ref}>
-      <div className="workspace-inner" ref={parent}>
+      <div
+        className="workspace-inner"
+        style={
+          dims ? { ...dims, maxWidth: dims.width, margin: "0 auto" } : null
+        }
+        ref={parent}
+      >
         <LayerSet namespace="image" ref={imageLayerSet.refs.rootRef}>
           <Layer
             ref={imageLayerSet.refs.backgroundRef}
             z={0}
-            width={imageLayerSet.width ?? 100}
-            height={imageLayerSet.height ?? 100}
+            width={dims?.width ?? imageLayerSet.width ?? 100}
+            height={dims?.height ?? imageLayerSet.height ?? 100}
           />
           <Layer
             ref={imageLayerSet.refs.foregroundRef}
             z={1}
-            width={imageLayerSet.width ?? 100}
-            height={imageLayerSet.height ?? 100}
+            width={dims?.width ?? imageLayerSet.width ?? 100}
+            height={dims?.height ?? imageLayerSet.height ?? 100}
           />
         </LayerSet>
         <LayerSet namespace="text" ref={textLayerSet.refs.rootRef}>
           <Layer
             ref={textLayerSet.refs.backgroundRef}
             z={0}
-            width={textLayerSet.width ?? 100}
-            height={textLayerSet.height ?? 100}
+            width={dims?.width ?? textLayerSet.width ?? 100}
+            height={dims?.height ?? textLayerSet.height ?? 100}
           />
           <Layer
             ref={textLayerSet.refs.foregroundRef}
             z={1}
-            width={textLayerSet.width ?? 100}
-            height={textLayerSet.height ?? 100}
+            width={dims?.width ?? textLayerSet.width ?? 100}
+            height={dims?.height ?? textLayerSet.height ?? 100}
           />
         </LayerSet>
       </div>
